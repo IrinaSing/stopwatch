1f7eeff24274a169517784ca1d38b67d906df997
IrinaSing <irinasingh.mail@yandex.ru>
2021-08-03 15:35:13 +0200

    add laps

--- javascript/stopwatch.js
+++ javascript/stopwatch.js
@@ -4,6 +4,31 @@ let hours = 000,
   millisec = 0000,
   count = 0;
 
+// prepare for display with leading zeros
+const addZero = (num) => {
+  if (num < 10) {
+    num = "0" + num;
+    return num;
+  } else {
+    return num;
+  }
+};
+
+const dispMillis = (num) => {
+  if (num < 1000 && num > 99) {
+    num = "0" + num;
+    return num;
+  } else if (num < 99 && num > 9) {
+    num = "00" + num;
+    return num;
+  } else if (num < 10) {
+    num = "0" + num;
+    return num;
+  } else {
+    return num;
+  }
+};
+
 function newtimer() {
   hours = parseInt((count * 10) / 1000 / 60 / 60); // calculate hours
   min = parseInt((count * 10) / 1000 / 60); // calculate minutes
@@ -11,31 +36,6 @@ function newtimer() {
   millisec = parseInt((count * 10) % 1000); // calculate milliseconds
 
   count++; // increment in count.
-
-  // prepare for display with leading zeros
-  const addZero = (num) => {
-    if (num < 10) {
-      num = "0" + num;
-      return num;
-    } else {
-      return num;
-    }
-  };
-
-  const dispMillis = (num) => {
-    if (num < 1000 && num > 99) {
-      num = "0" + num;
-      return num;
-    } else if (num < 99 && num > 9) {
-      num = "00" + num;
-      return num;
-    } else if (num < 10) {
-      num = "0" + num;
-      return num;
-    } else {
-      return num;
-    }
-  };
 
   document.getElementById("milliseconds").innerHTML = dispMillis(millisec);
   document.getElementById("seconds").innerHTML = addZero(sec);
@@ -73,13 +73,37 @@ const reset = (e) => {
     millisec = 0000;
     count = 0;
 
-    document.getElementById("milliseconds").innerHTML = "0000";
-    document.getElementById("seconds").innerHTML = "00";
-    document.getElementById("minutes").innerHTML = "00";
-    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("milliseconds").innerHTML = dispMillis(millisec);
+    document.getElementById("seconds").innerHTML = addZero(sec);
+    document.getElementById("minutes").innerHTML = addZero(min);
+    document.getElementById("hours").innerHTML = addZero(hours);
   }
 };
 
 const resetBtn = document.getElementById("btn-reset");
 resetBtn.addEventListener("click", reset);
 resetBtn.addEventListener("click", toggle);
+
+let lapNow = null;
+
+const addLap = (e) => {
+  const target = e.target;
+
+  const h = addZero(hours);
+  const m = addZero(min);
+  const s = addZero(sec);
+  const mi = millisec;
+
+  if (target.id === "btn-add-lap") {
+    lapNow = `${h}:${m}:${s}.${mi}`;
+
+    const ulEl = document.getElementById("laps-list");
+    const newLi = document.createElement("LI");
+    newLi.innerHTML = ++ulEl.children.length + ". " + lapNow;
+
+    ulEl.appendChild(newLi);
+  }
+};
+
+const lapBtn = document.getElementById("btn-add-lap");
+lapBtn.addEventListener("click", addLap);
